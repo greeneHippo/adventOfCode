4acf5522ab74a57e7523ad315db56ac6b520a30d
Joost Groen <joost@yoink.nl>
2024-01-17 16:18:04 +0100

    Day 24

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.6")
     implementation("org.apache.commons:commons-math3:3.6.1")
+    implementation("org.jetbrains.kotlinx:multik-core:0.2.2")
+    implementation("org.jetbrains.kotlinx:multik-default:0.2.2")
     testImplementation(kotlin("test"))
 }
 
