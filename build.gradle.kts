af039f0c4785b55592aae63f2ed773052f063da8
Joost Groen <joost@yoink.nl>
2026-01-05 15:49:39 +0100

    Upgrade Java, Kotlin and Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "2.3.0"
 }
 
 group = "nl.groen"
@@ -10,10 +10,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.commons:commons-lang3:3.6")
+    implementation("org.apache.commons:commons-lang3:3.18.0")
     implementation("org.apache.commons:commons-math3:3.6.1")
-    implementation("org.jetbrains.kotlinx:multik-core:0.2.2")
-    implementation("org.jetbrains.kotlinx:multik-default:0.2.2")
+    implementation("org.jetbrains.kotlinx:multik-core:0.2.3")
+    implementation("org.jetbrains.kotlinx:multik-default:0.2.3")
     testImplementation(kotlin("test"))
 }
 
@@ -22,5 +22,5 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
