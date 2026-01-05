2f9c83a97a564671aaaf76ce3efef25e46869386
Joost Groen <joost@yoink.nl>
2026-01-05 16:19:38 +0100

    Refactor 2023_1

--- src/main/kotlin/nl/groen/year2023/day1/Day1.kt
+++ src/main/kotlin/nl/groen/year2023/day1/Day1.kt
@@ -1,41 +1,58 @@
 package nl.groen.year2023.day1
 
+import nl.groen.println
+import nl.groen.readInput
 import org.apache.commons.lang3.StringUtils
-import java.io.File
-import java.io.FileReader
 
-fun main(args : Array<String>) {
+fun main() {
 
-    var file = File("C:\\Workspace\\AdventOfCode2023\\src\\main\\resources\\day1_input.txt")
+    fun solution (input : List<String>, includeWords: Boolean): Long {
 
-    var fileReader = FileReader(file)
-    var lines = fileReader.readLines()
+        val digits = input.stream().map {
+            val characters = it.toCharArray()
+            val twoDigitString = CharArray(2)
+            twoDigitString[0] = extractFistDigit(characters, includeWords)
+            twoDigitString[1] = extractLastDigit(characters, includeWords)
 
-    var digits = lines.stream().map{
-        var characters = it.toCharArray()
-        var twoDigitString = CharArray(2)
-        twoDigitString[0] = extractFistDigit(characters)
-        twoDigitString[1] = extractLastDigit(characters)
+            return@map twoDigitString.concatToString().toInt()
+        }
 
-        return@map twoDigitString.concatToString().toInt()
+        val sum = digits.reduce{sum, element -> sum + element}
+        return sum.get().toLong()
     }
 
-//    for (digit in digits) {
-//        println(digit)
-//    }
-    var sum = digits.reduce{sum, element -> sum + element}
-    println(sum.get())
+    fun part1 (input : List<String>): Long {
+        return solution(input, false)
+    }
 
+    fun part2 (input : List<String>): Long {
+        return solution(input, true)
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("2023","Day01_test")
+    val testInput2 = readInput("2023","Day01_test2")
+    val input = readInput("2023","Day01")
+
+    check(part1(testInput) == 142L)
+    part1(input).println()
+
+    check(part2(testInput2) == 281L)
+    part2(input).println()
 }
 
-private fun extractFistDigit(characters: CharArray): Char {
+private fun extractFistDigit(characters: CharArray, includeWords: Boolean): Char {
     for (i in characters.indices) {
 
         if (characters[i].isDigit()) {
             return characters[i]
         }
 
-        var digitInWord = digitInWord(i, characters.concatToString())
+        if (!includeWords) {
+            continue
+        }
+
+        val digitInWord = digitInWord(i, characters.concatToString())
         if (digitInWord != null) {
             return digitInWord
         }
@@ -45,13 +62,17 @@ private fun extractFistDigit(characters: CharArray): Char {
     throw RuntimeException("No digit")
 }
 
-private fun extractLastDigit(characters: CharArray): Char {
+private fun extractLastDigit(characters: CharArray, includeWords: Boolean): Char {
     for (i in characters.indices) {
         if (characters[characters.size-i-1].isDigit()) {
             return characters[characters.size-i-1]
         }
 
-        var digitInWord = digitInWordRecursive(characters.size - i, characters.concatToString())
+        if (!includeWords) {
+            continue
+        }
+
+        val digitInWord = digitInWordRecursive(characters.size - i, characters.concatToString())
         if (digitInWord != null) {
             return digitInWord
         }
@@ -61,31 +82,31 @@ private fun extractLastDigit(characters: CharArray): Char {
 }
 
 private fun digitInWord(i: Int, string: String): Char? {
-    if (StringUtils.equals("one", StringUtils.substring(string, i, i+3))) {
+    if ("one" == StringUtils.substring(string, i, i+3)) {
         return "1".toCharArray()[0]
     }
-    if (StringUtils.equals("two", StringUtils.substring(string, i, i+3))) {
+    if ("two" == StringUtils.substring(string, i, i+3)) {
         return "2".toCharArray()[0]
     }
-    if (StringUtils.equals("three", StringUtils.substring(string, i, i+5))) {
+    if ("three" == StringUtils.substring(string, i, i+5)) {
         return "3".toCharArray()[0]
     }
-    if (StringUtils.equals("four", StringUtils.substring(string, i, i+4))) {
+    if ("four" == StringUtils.substring(string, i, i+4)) {
         return "4".toCharArray()[0]
     }
-    if (StringUtils.equals("five", StringUtils.substring(string, i, i+4))) {
+    if ("five" == StringUtils.substring(string, i, i+4)) {
         return "5".toCharArray()[0]
     }
-    if (StringUtils.equals("six", StringUtils.substring(string, i, i+3))) {
+    if ("six" == StringUtils.substring(string, i, i+3)) {
         return "6".toCharArray()[0]
     }
-    if (StringUtils.equals("seven", StringUtils.substring(string, i, i+5))) {
+    if ("seven" == StringUtils.substring(string, i, i+5)) {
         return "7".toCharArray()[0]
     }
-    if (StringUtils.equals("eight", StringUtils.substring(string, i, i+5))) {
+    if ("eight" == StringUtils.substring(string, i, i+5)) {
         return "8".toCharArray()[0]
     }
-    if (StringUtils.equals("nine", StringUtils.substring(string, i, i+4))) {
+    if ("nine" == StringUtils.substring(string, i, i+4)) {
         return "9".toCharArray()[0]
     }
 
@@ -93,31 +114,31 @@ private fun digitInWord(i: Int, string: String): Char? {
 }
 
 fun digitInWordRecursive(i: Int, string: String): Char? {
-    if (StringUtils.equals("one", StringUtils.substring(string, i-3, i))) {
+    if ("one" == StringUtils.substring(string, i-3, i)) {
         return "1".toCharArray()[0]
     }
-    if (StringUtils.equals("two", StringUtils.substring(string, i-3, i))) {
+    if ("two" == StringUtils.substring(string, i-3, i)) {
         return "2".toCharArray()[0]
     }
-    if (StringUtils.equals("three", StringUtils.substring(string, i-5, i))) {
+    if ("three" == StringUtils.substring(string, i-5, i)) {
         return "3".toCharArray()[0]
     }
-    if (StringUtils.equals("four", StringUtils.substring(string, i-4, i))) {
+    if ("four" == StringUtils.substring(string, i-4, i)) {
         return "4".toCharArray()[0]
     }
-    if (StringUtils.equals("five", StringUtils.substring(string, i-4, i))) {
+    if ("five" == StringUtils.substring(string, i-4, i)) {
         return "5".toCharArray()[0]
     }
-    if (StringUtils.equals("six", StringUtils.substring(string, i-3, i))) {
+    if ("six" == StringUtils.substring(string, i-3, i)) {
         return "6".toCharArray()[0]
     }
-    if (StringUtils.equals("seven", StringUtils.substring(string, i-5, i))) {
+    if ("seven" == StringUtils.substring(string, i-5, i)) {
         return "7".toCharArray()[0]
     }
-    if (StringUtils.equals("eight", StringUtils.substring(string, i-5, i))) {
+    if ("eight" == StringUtils.substring(string, i-5, i)) {
         return "8".toCharArray()[0]
     }
-    if (StringUtils.equals("nine", StringUtils.substring(string, i-4, i))) {
+    if ("nine" == StringUtils.substring(string, i-4, i)) {
         return "9".toCharArray()[0]
     }
 
